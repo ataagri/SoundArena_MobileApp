ff61d1fb73a3c3c75898796b478360f6bb6739ed
mucchick <mucchick@gmail.com>
2023-11-13 21:24:36 +0300

    Login/Register Binding

--- app/src/main/java/com/example/cs308_mobileapplication/registerAct.kt
+++ app/src/main/java/com/example/cs308_mobileapplication/registerAct.kt
@@ -1,6 +1,8 @@
 package com.example.cs308_mobileapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +13,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cs308_mobileapplication.ui.theme.CS308_MobileApplicationTheme
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterAct : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registeract)
+        //Sign In Button Switches to Login Page
+        val signInText = findViewById<TextView>(R.id.HaveAcc)
+        signInText.setOnClickListener {
+            val toSignIn = Intent(this, LoginAct::class.java)
+            startActivity(toSignIn)
+        }
+
     }
+}
+
+class LoginAct : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginact)
+
+        val signUpText = findViewById<TextView>(R.id.SignUp)
+        signUpText.setOnClickListener {
+            val toSignup = Intent(this, RegisterAct::class.java)
+            startActivity(toSignup)
+        }
+
+
+    }
+
 }
 
 
