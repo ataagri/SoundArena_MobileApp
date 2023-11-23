fd29c1708f57ef59ac1389beaebe93832be31fd8
mucchick <mucchick@gmail.com>
2023-11-23 14:07:47 +0300

    Backend Connection for Register

--- app/src/main/java/com/example/cs308_mobileapplication/registerAct.kt
+++ app/src/main/java/com/example/cs308_mobileapplication/registerAct.kt
@@ -15,6 +15,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.PUT
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
 
 
 //Creating Retrofit INSTANCE
@@ -28,9 +30,18 @@ interface UserService {
 object RetrofitClient {
     private const val SERVER_URL = "http://10.0.2.2:3000/"
 
-    val instance: UserService by lazy{
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val instance: UserService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(SERVER_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
